cacc0b7b8ccb71d79fcd5a213e9481a405c1ca5d
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2024-11-17 22:45:12 +0500

    leetcode

---
+++ leetcode/leedcode/35.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func inputArray(arr []int, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+}
+
+func searchInsert(arr []int, target int) int {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] >= target {
+			return i
+		}
+	}
+	return len(arr)
+}
+
+func main() {
+	var n, target int
+	fmt.Scan(&n, &target)
+	arr := make([]int, n)
+	inputArray(arr, n)
+	fmt.Print(searchInsert(arr, target))
+}
--- leetcode/main.go
+++ leetcode/main.go
@@ -1,8 +1,38 @@
-package main 
-
+package main
 
 import "fmt"
 
-func main() { 
+func inputNum1(nums1 []int, m int) {
+	for i := 0; i < m; i++ {
+		fmt.Scan(&nums1[i])
+	}
+}
+func inputNum2(nums2 []int, n int) {
+	for i :=0; i < n; i++ {
+		fmt.Scan(&nums2[i])
+	}
+}
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for i := 0; i < n + m; i++ {
+		nums1 = append(nums1, nums2[i])
+	}
+}
+
+func output(nums1 []int, n int, m int) {
+	for i:=0;i < n+m; i++ {
+		fmt.Print(nums1[i], " ")
+	}
+}
+
+func main() {
+	var n, m int
+	fmt.Scan(&m, &n)
+
 	
+	nums1 := make([]int, m)
+	nums2 := make([]int, n)
+	inputNum1(nums1, m)
+	inputNum2(nums2, n)
+	merge(nums1, m, nums2, n)
+	output(nums1, n, m)
 }

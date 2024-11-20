b2d2e8044c395f1f7d7c09d535e91dbe642e5f88
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2024-11-20 20:37:47 +0500

    for

--- leetcode/main.go
+++ leetcode/main.go
@@ -6,7 +6,7 @@ import "fmt"
 func main() { 
 	numbers := []int{2,3,4}
 
-	for _, value := range numbers {
-		fmt.Println(value)
+	for index, value := range numbers {
+		fmt.Println(index, " ", value)
 	}
 }

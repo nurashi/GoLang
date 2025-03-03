a30731b999b808e295a81e0dbc594d05b048f3f1
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-03-03 12:29:12 +0500

    Implement isValid function and add maxProductDifference to new file

--- leetcode/array1/main.go
+++ leetcode/array1/main.go
@@ -1,27 +1,28 @@
 package main
 
 import "fmt"
-func maxProductDifference(nums []int) int {
-//	sort.Ints(nums)
-//	return nums[len(nums)-1]*nums[len(nums)-2] - (nums[0] * nums[1])
-	var tempMax int
-	var tempMin int
-	max := -1
-	min := 50000
-	for _, i := range nums {
-		if i > max {
-			tempMax = max
-			max = i
-		}
-		if i < min {
-			tempMin = min
-			min = i
+
+func isValid(s string) bool {
+
+	helper := false
+
+	if len(s) <= 1 {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+			if s[i]+2 == s[j] || s[i]+1 == s[j] {
+				helper = true
+			}
 		}
 	}
-	return tempMax*max - tempMin*min
+
+	return helper
 }
 
 func main() {
-	nums := []int{5, 6, 2, 7, 4}
-	fmt.Println(maxProductDifference(nums))
+	s := "()"
+
+	fmt.Println(isValid(s))
 }

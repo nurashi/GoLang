2fd59190573a604c88515ae27b3cfefd56be3f44
Nurasyl Orazbek <155213060+nurashi@users.noreply.github.com>
2025-04-18 18:00:17 +0500

    Create 1925.go

---
+++ leetcode/leedcode/1925.go
@@ -0,0 +1,16 @@
+package leedcode
+
+func countTriples(n int) int {
+	result := 0
+	for n > 0 {
+		for i := 1; i < n; i++ {
+			tmp := (n*n) - (i*i)
+			r := int(math.Sqrt(float64(tmp)))
+			if r*r == tmp {
+				result++
+			}
+		}
+		n--
+	}
+	return result
+}

a062c19bb4bccc6dd53e27d0c64542dead17455e
Nurasyl Orazbek <155213060+nurashi@users.noreply.github.com>
2025-04-29 15:07:50 +0500

    700 task

---
+++ leetcode/leedcode/700.go
@@ -0,0 +1,30 @@
+package leedcode
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+
+	for root != nil {
+		if val == root.Val {
+			return root
+	
+    	} else if val < root.Val {
+    		root = root.Left
+	
+    	} else {
+			root = root.Right
+	
+    	}
+	}
+
+	return nil
+}

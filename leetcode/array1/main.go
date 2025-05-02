bb5e09038dbc5269be762e87c0322cf0cf69be37
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-05-02 20:01:42 +0500

    2974.go/387.go

--- leetcode/array1/main.go
+++ leetcode/array1/main.go
@@ -1,27 +1,38 @@
-package main 
+package main
 
-func cheker(word string, letter string) bool {
-	count := 0
-	for i := 0; i < len(word); i++ {
-		if letter == string(word[i]) {
-			count++
-		}
+import (
+	"fmt"
+	"slices"
+)
+
+
+//[5 4 2 3] -> [3 2 5 4]
+// first round alice removes 2, Bob removes 3, append -> 3 and then 2, alice, bob, bob, alice
+
+// find min first time, and remove, but save in some varible ALICE 
+// find min second time, and remove, but save in same varible BOB
+// append BOB varible to result
+// append ALICE variable to result
+func numberGame(nums []int) []int {
+
+	result := []int{}
+
+	// 2 3 4 5 -> 3 2 5 4
+	slices.Sort(nums)
+	for i := 1; i < len(nums); i+=2 {
+		result = append(result, nums[i])
+		result = append(result, nums[i - 1])
 	}
-	return count == 1
-}
 
-
-func firstUniqChar(s string) int {
 	
-	for i := 0; i < len(s); i++{
-		if(cheker(s, string(s[i]))){
-			return i;
-		}
-	}
-	return -1;
+	
+	return result
 }
 
-func main() { 
-	firstUniqChar("leetcode");
+func main() {
+	nums := []int{5,4,2,3}
 
+	result := numberGame(nums)
+
+	fmt.Print(result, " ")
 }

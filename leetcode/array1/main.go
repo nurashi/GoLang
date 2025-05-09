dd96bd395bdc2e6b040c3146c3b6c486a9d81303
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-05-09 12:14:33 +0500

    709.go

--- leetcode/array1/main.go
+++ leetcode/array1/main.go
@@ -1,23 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 )
 
-func countDigits(num int) int {
-    strNum := strconv.Itoa(num)
-	counter := 0
-	for i := 0; i < len(strNum); i++ {
-		current := int(strNum[i])
-		if(num % current == 0){
-			counter++
-		}
-	}
-	return counter + 1;
-}
 
 func main() { 
-	num := 121
-	fmt.Println(countDigits(num))
 }

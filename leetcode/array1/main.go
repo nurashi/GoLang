85c0a9f5fcbf3c355e5e439033b288c49bce0069
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-05-11 21:18:11 +0500

    709

--- leetcode/array1/main.go
+++ leetcode/array1/main.go
@@ -1,8 +1,32 @@
 package main
 
-import (
-)
+import "fmt"
 
+func sortedSquares(nums []int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		nums[i] = nums[i] * nums[i]
+	}
+
+
+	for i := 0; i < len(nums) - 1; i++ {
+		for j := 0; j < len(nums) - i - 1; j++ {
+			if(nums[j] > nums[j + 1]){
+				temp := nums[j]
+				nums[j] = nums[j + 1]
+				nums[j + 1] = temp
+			}
+		}
+	}
+	return nums
+}
 
 func main() { 
+
+	nums := []int{-4,-1,0,3,10}
+	sortedSquares(nums)
+
+	fmt.Println(nums)
+
+
 }

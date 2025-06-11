47312ade7442e3ad2a67812f59ce3e436f8be03d
nurashi <nurashi778@gmail.com>
2025-06-11 09:06:25 +0500

    1672

--- leetcode/array1/main.go
+++ leetcode/array1/main.go
@@ -1,26 +1,6 @@
-package main
+package main 
 
-import "fmt"
 
-func maximumWealth(accounts [][]int) int {
-	max := 0
-	checkMax := 0
-	for i := 0; i < len(accounts); i++ {
-		max = 0
-		for j := 0; j < len(accounts[i]); j++ {
-			max += accounts[i][j]
-		}
-		if max > checkMax {
-			checkMax = max
-		}
-	}
-
-	return checkMax
-}
-
-func main() {
-
-	arr := [][]int{{1, 2, 3}, {3, 2, 1}}
-
-	fmt.Print(maximumWealth(arr))
+func main() { 
+	
 }
